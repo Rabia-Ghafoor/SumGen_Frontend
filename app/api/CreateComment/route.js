e3263d36175897fd4eb701441d5c914fac974b0c
Salif Tankoano <tanksalif@gmail.com>
2024-08-25 18:26:32 -0400

    add basic route structure

---
+++ app/api/CreateComment/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  const data = await req.json();
+  const results = await index.query({
+    topK: 3,
+    includeMetadata: true,
+    vector: embedding.data[0].embedding,
+  });
+
+  return new NextResponse(stream);
+}
